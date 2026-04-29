56549ad2bffce629ad092a852da280c182cf7dd1
srsohan485 <sayedurrahmansohan@gmail.com>
2026-04-29 20:21:20 +0600

    Sirialize Data

--- api/views.py
+++ api/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 
 def studentsView(request):
-    students = {
-            'id': 1,
-            'name': 'Sayedur',
-            'class': 'Computer Science'
-        }
-    return JsonResponse(students)  # ← safe=False যোগ করুন , safe=False
+    students = Student.objects.all()
+    students_list = list(students.values())
+    return JsonResponse(students_list, safe=False)  # ← safe=False যোগ করুন , safe=False
