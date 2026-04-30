87fe6422285e1f9a30c668a89f554416bd210fad
srsohan485 <sayedurrahmansohan@gmail.com>
2026-04-30 16:32:14 +0600

    Post Api Created

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
-#from django.shortcuts import render
-#from django.http import JsonResponse
 from students.models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def studentsView(request):
     if request.method == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
