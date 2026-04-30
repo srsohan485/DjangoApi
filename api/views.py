8211ac8493737e03212b83faf8b2cc801a61cdbc
srsohan485 <sayedurrahmansohan@gmail.com>
2026-04-30 15:56:19 +0600

    create url and serializers

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def studentsView(request):
-    students = Student.objects.all()
-    students_list = list(students.values())
-    return JsonResponse(students_list, safe=False)  # ← safe=False যোগ করুন , safe=False
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
