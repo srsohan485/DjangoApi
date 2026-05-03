6d5420506ccef33f4343631fd82201ed82aa9493
srsohan485 <sayedurrahmansohan@gmail.com>
2026-05-03 05:49:02 -0700

    Put Api calling

--- api/views.py
+++ api/views.py
@@ -63,3 +63,16 @@ class EmployeeDetail(APIView):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
