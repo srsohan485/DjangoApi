b72b93ec1047cdbafee8d374b7762ac0592597a4
srsohan485 <sayedurrahmansohan@gmail.com>
2026-05-03 00:52:51 -0700

    Employes Serializers

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('students/', views.studentsView),        
     path('students/<int:pk>/', views.studentDetailView),
+    path('employees/', views.Employees.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from students.models import Student
-from .serializers import StudentSerializer
+from employes.models import Employee 
+from .serializers import StudentSerializer, EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
@@ -39,3 +41,9 @@ def studentDetailView(request, pk):
     elif request.method == 'DELETE':
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
