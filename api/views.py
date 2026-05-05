8ace67bc74fd4c9a66949fe09ed56a8d6d60a0aa
srsohan485 <sayedurrahmansohan@gmail.com>
2026-05-05 13:14:23 +0600

    Custom Filtaring and Smale Leter Filter

--- api/views.py
+++ api/views.py
@@ -11,6 +11,8 @@ from rest_framework import viewsets
 from blogs.serializers import BlogSerializer, CommentSerializer
 from blogs.models import Blog,Comment
 from .pagination import CustomPagination
+from employes.filters import EmployeeFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
@@ -159,7 +161,8 @@ class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer   
     pagination_class = CustomPagination   
-    filterset_fields = ['designation']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = EmployeeFilter
 
 class BlogsView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
---
+++ employes/filters.py
@@ -0,0 +1,9 @@
+import django_filters
+from .models import Employee
+
+class EmployeeFilter(django_filters.FilterSet):
+    designation = django_filters.CharFilter(field_name='designation',lookup_expr='iexact')
+
+    class Meta:
+        model = Employee
+        fields = ['designation']
