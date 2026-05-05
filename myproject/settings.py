f179b749595e43d4096e055299a4f07ff9fda1f4
srsohan485 <sayedurrahmansohan@gmail.com>
2026-05-05 10:22:55 +0600

    add pagination

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
